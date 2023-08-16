1f35b60492129e1223db0e24910cdf8c1c0b504a
VirLg <108211942+VirLg@users.noreply.github.com>
2023-08-16 22:14:26 +0300

    add

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -1,4 +1,4 @@
-import { combineReducers } from 'redux';
+// import { combineReducers } from 'redux';
 import { contactSlice } from './slice';
 import { sortSlice } from './sliceFilter';
 export const reducer = {
