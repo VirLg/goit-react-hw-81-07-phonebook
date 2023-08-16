1316258aa6c8d2e4d18d707bad87ed22ca07f7dd
VirLg <108211942+VirLg@users.noreply.github.com>
2023-08-16 22:12:01 +0300

    add

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -1,7 +1,7 @@
 import { combineReducers } from 'redux';
 import { contactSlice } from './slice';
 import { sortSlice } from './sliceFilter';
-export const reducer = combineReducers({
+export const reducer = {
   contactsBook: contactSlice.reducer,
   contactFilter: sortSlice.reducer,
-});
+};
--- src/redux/store.js
+++ src/redux/store.js
@@ -2,19 +2,14 @@ import { configureStore } from '@reduxjs/toolkit';
 
 // import { contactSlice } from './slice';
 // import { sortSlice } from './sliceFilter';
-import { persistStore, persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
+
+
 import { reducer } from './reducers';
 
-const persistConfig = {
-  key: 'contact',
-  storage,
-};
 
-const persistedReducer = persistReducer(persistConfig, reducer);
 
-export const store = configureStore({ reducer: persistedReducer });
-export const persistor = persistStore(store);
+export const store = configureStore({ reducer });
+
 // export const store = configureStore({
 //   reducer: {
 //     addContacts: contactSlice.reducer,
